a7f9c2b03196259c9818b18745185a460b181913
Burton Smith <burton@breakstuff.io>
2022-08-05 11:37:34 -0400

    localize type

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,12 @@
 import { Plugin } from "@custom-elements-manifest/analyzer";
-import type { Options } from "./src/types";
+
+export interface Options {
+  /** path to output directory */
+  outdir?: string;
+  /** name of the file with you component's custom data */
+  filename?: string;
+  /** class names of any components you would like to exclude from the custom data */
+  exclude?: string[];
+}
 
 export declare function generateCustomData(options: Options): Plugin;
