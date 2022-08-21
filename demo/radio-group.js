606d06f7cf08d9ecb4b167c890f4ba7a1651b27f
Burton Smith <burton@breakstuff.io>
2022-08-20 21:49:54 -0400

    update examples

---
+++ demo/radio-group.js
@@ -0,0 +1,40 @@
+/**
+ *
+ * Radio groups are used to group multiple radios or radio buttons so they function as a single form control. Here is its [documentation](https://github.com/microsoft/vscode-custom-data/blob/master/samples/webcomponents/src/components/my-component/docs.md).
+ *
+ * Use it like this:
+ * ```html
+ * <radio-group value="2" size="3">
+ *   <span slot="label">My Label</span>
+ *   <radio-button value="1">Option 1</radio-button>
+ *   <radio-button value="2">Option 2</radio-button>
+ *   <radio-button value="3">Option 3</radio-button>
+ * </radio-group>
+ * ```
+ *
+ * @tag radio-group
+ * @tagname radio-group
+ *
+ * @attr {boolean} disabled - Disables the element
+ * @attribute {string} value - The value of the selected radio
+ * @attribute {1|2|3|4} size - This will control the size of radio buttons
+ *
+ * @csspart bar - Styles the color of bar
+ *
+ * @slot - add radio buttons to the `default` slot to create options to your radio group
+ * @slot label - placeholder for the radio group label
+ *
+ * @cssprop --text-color - Controls the color of foo
+ * @cssproperty [--background-color=red] - Controls the color of bar
+ *
+ * @prop {boolean} prop1 - this toggles some unseen feature
+ * @property {number} prop2 - this will adjust thr width of the unit
+ *
+ * @fires custom-event - some description for custom-event
+ * @fires {Event} typed-event - some description for typed-event
+ * @event {CustomEvent} typed-custom-event - some description for typed-custom-event
+ *
+ */
+class RadioGroup extends HTMLElement {}
+
+customElements.define("radio-group", RadioGroup);
