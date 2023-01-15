620f4331df31caaec580b49528ae14290313ff21
Burton Smith <burton@breakstuff.io>
2023-01-15 10:30:52 -0500

    update jest to support esm

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,6 +1,18 @@
+// eslint-disable-next-line no-undef
 module.exports = {
-  transform: { "^.+\\.ts?$": "ts-jest" },
+  transform: {
+    "^.+\\.ts?$": [
+      "ts-jest",
+      {
+        useESM: true,
+      },
+    ],
+  },
   testEnvironment: "node",
   testRegex: "/.*\\.(test|spec)?\\.(ts)$",
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  moduleFileExtensions: ["ts", "js", "json", "node"],
+  extensionsToTreatAsEsm: [".ts"],
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
 };
