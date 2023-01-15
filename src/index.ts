2cae7e94f30868cd492bea980875800ae21d0abf
Burton Smith <burton@breakstuff.io>
2023-01-15 10:35:00 -0500

    update imports

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,27 @@
-import {
-  generateCustomDataFile,
-  updateConfig,
-} from "./custom-data-generator/generator.js";
-import { setComponentReferences } from "./custom-data-generator/cem-utilities.js";
-import type { Options, Params } from "../types";
+import { updateConfig } from "./custom-data-generator/cem-utilities.js";
+import { setComponentReferences } from "./custom-data-generator/references.js";
 import { greenConsoleLog } from "./custom-data-generator/integrations.js";
+import { generateCustomDataFile } from "./custom-data-generator/data-file-generator.js";
+import type { Options, Params } from "../types";
 
 export function generateCustomData(params: Options = {}) {
   updateConfig(params);
 
   return {
     name: "cem-plugin-vs-code-custom-data-generator",
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
     analyzePhase({ ts, node, moduleDoc }) {
       setComponentReferences(ts, node, moduleDoc);
     },
     packageLinkPhase({ customElementsManifest }: Params) {
-      console.log("[vs-code-custom-data-generator] - Generating config files...");
+      console.log(
+        "[vs-code-custom-data-generator] - Generating config files..."
+      );
       generateCustomDataFile(customElementsManifest);
-      greenConsoleLog("[vs-code-custom-data-generator] - File generation complete.");
+      greenConsoleLog(
+        "[vs-code-custom-data-generator] - File generation complete."
+      );
     },
   };
 }
